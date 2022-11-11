099a280a37693951d884e4cffe5d8d7cc312c75a
Miriam Gordillo-Chavez <miriam.gordillo-chavez@united.com>
2022-11-11 14:19:49 -0600

    using useCallback, and useMemo, React batching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Button from './components/UI/Button/Button'
 
 import './App.css';
+import DemoOutput from './components/UI/Button/Demo/DemoOutput';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('app running');
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+  
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
+  
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler} >Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler} >Toggle Paragraph!</Button>
     </div>
   );
 }
